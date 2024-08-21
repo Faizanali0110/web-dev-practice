636226ff2f84d7ece3978ba0e5d273aafd9f5fd3
unknown <01-131222-017@student.bahria.edu.pk>
2024-08-21 22:12:01 +0500

    add TextAnimation folder

---
+++ TextAnimation/TextAnimation.js
@@ -0,0 +1,18 @@
+const text=document.querySelector(".animation-text")
+
+
+
+setInterval(() => {
+    text.textContent="Student"
+    
+}, 4000);
+
+setInterval(() => {
+    text.textContent="Engineer"
+    
+}, 8000);
+
+setInterval(() => {
+    text.textContent="DEV"
+    
+}, 12000);
